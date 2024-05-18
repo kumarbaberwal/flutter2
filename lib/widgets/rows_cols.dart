06b2e4a2640ae2c7e9d55e866a82bd21d784c586
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-05-19 01:32:13 +0530

    rows and columns

--- lib/widgets/rows_cols.dart
+++ lib/widgets/rows_cols.dart
@@ -10,15 +10,18 @@ class RowsCols extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.lightBlue,
-        title: const Text("Rows Cols Wrap"),
+        centerTitle: true,
+        title: const Text("Rows Cols Wrap", style: TextStyle(fontWeight: FontWeight.bold)),
       ),
       body: Container(
         height: h,
         width: w,
         color: Colors.pink[200],
-        child: Row(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment: CrossAxisAlignment.center,
+          // alignment: WrapAlignment.center,
+          // direction: Axis.vertical,
           children: [
             Container(
               height: 50,
@@ -76,7 +79,68 @@ class RowsCols extends StatelessWidget {
                   ]),
             ),
           ],
-        ),
+        )
+        // Row(
+        //   mainAxisAlignment: MainAxisAlignment.spaceAround,
+        //   crossAxisAlignment: CrossAxisAlignment.center,
+        //   children: [
+        //     Container(
+        //       height: 50,
+        //       width: 50,
+        //       decoration: const BoxDecoration(
+        //           color: Colors.black,
+        //           // border: Border.all(color: Colors.white, width: 4),
+        //           boxShadow: [
+        //             BoxShadow(
+        //                 color: Colors.white, blurRadius: 5, spreadRadius: 8),
+        //           ]),
+        //     ),
+        //     Container(
+        //       height: 50,
+        //       width: 50,
+        //       decoration: const BoxDecoration(
+        //           color: Colors.purple,
+        //           // border: Border.all(color: Colors.white, width: 4),
+        //           boxShadow: [
+        //             BoxShadow(
+        //                 color: Colors.white, blurRadius: 5, spreadRadius: 8),
+        //           ]),
+        //     ),
+        //     Container(
+        //       height: 50,
+        //       width: 50,
+        //       decoration: const BoxDecoration(
+        //           color: Colors.red,
+        //           // border: Border.all(color: Colors.white, width: 4),
+        //           boxShadow: [
+        //             BoxShadow(
+        //                 color: Colors.white, blurRadius: 5, spreadRadius: 8),
+        //           ]),
+        //     ),
+        //     Container(
+        //       height: 50,
+        //       width: 50,
+        //       decoration: const BoxDecoration(
+        //           color: Colors.brown,
+        //           // border: Border.all(color: Colors.white, width: 4),
+        //           boxShadow: [
+        //             BoxShadow(
+        //                 color: Colors.white, blurRadius: 5, spreadRadius: 8),
+        //           ]),
+        //     ),
+        //     Container(
+        //       height: 50,
+        //       width: 50,
+        //       decoration: const BoxDecoration(
+        //           color: Colors.yellow,
+        //           // border: Border.all(color: Colors.white, width: 4),
+        //           boxShadow: [
+        //             BoxShadow(
+        //                 color: Colors.white, blurRadius: 5, spreadRadius: 8),
+        //           ]),
+        //     ),
+        //   ],
+        // ),
       ),
     );
   }
