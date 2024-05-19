863c93079e0e775ff04cc654278aa782cdf1daad
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-05-19 17:39:31 +0530

    button

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:first/widgets/rows_cols.dart';
+import 'package:first/widgets/button.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
         brightness: Brightness.light,
         primaryColor: Colors.deepPurple,
       ),
-      home: const RowsCols(),
+      home: const Button(),
     );
   }
 }
---
+++ lib/widgets/button.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class Button extends StatelessWidget {
+  const Button({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.lightBlue,
+        centerTitle: true,
+        // titleTextStyle: const TextStyle(fontWeight: FontWeight.bold),
+        title: const Text(
+          "Buttons",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+                style: ButtonStyle(
+                    elevation: MaterialStateProperty.all(20),
+                    padding:
+                        MaterialStateProperty.all(const EdgeInsets.all(20)),
+                    overlayColor: MaterialStateProperty.all(Colors.cyan),
+                    backgroundColor: MaterialStateProperty.all(Colors.amber)),
+                onPressed: () {},
+                child: const Text(
+                  "Press Me!",
+                  style: TextStyle(fontSize: 30, color: Colors.black),
+                )),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              height: 50,
+              width: 200,
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: const MaterialStatePropertyAll(Colors.black),
+                  overlayColor: const MaterialStatePropertyAll(Colors.white),
+                  shape: MaterialStatePropertyAll(RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ))
+                ),
+                  onPressed: () {
+                    print("Pressed");
+                  },
+                  child: const Text(
+                    "Press Me!",
+                    style: TextStyle(fontSize: 30, color: Colors.white),
+                  )),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
