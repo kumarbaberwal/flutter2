9f49fc1d72297c53b0c08ffadcebd5dc630cbcbd
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-05-28 09:36:46 +0530

    animated text

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:first/widgets/bottomsheet.dart';
+import 'package:first/widgets/animated_text.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
         brightness: Brightness.light,
         primaryColor: Colors.yellow,
       ),
-      home: const BottomSheetWidget(),
+      home: const AnimatedTextWidget(),
     );
   }
 }
