cb38cad7ccd6d38dc49d0db793ed24562273a35f
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-06-01 01:15:18 +0530

    image picker

--- lib/widgets/imagepicker.dart
+++ lib/widgets/imagepicker.dart
@@ -19,6 +19,32 @@ class _ImagePickerWidgetState extends State<ImagePickerWidget> {
           style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
         ),
       ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 300,
+              width: MediaQuery.of(context).size.width,
+              color: Colors.grey.shade200,
+              child: const Center(
+                child: Text('Image not Picked'),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                  backgroundColor:
+                      WidgetStatePropertyAll(Colors.green.shade300)),
+              child: const Text(
+                'Pick Image & Video',
+                style:
+                    TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
